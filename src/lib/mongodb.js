18089c737099e45e6293e6047027467fb18294a5
Subhangi priya <subhangipriya681@gmail.com>
2025-12-29 19:38:11 +0530

    fouth commit

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,7 +1,33 @@
+// import mongoose from "mongoose";
+
+// const MONGODB_URI = "mongodb+srv://subhangipriya681:Password@cluster0.qe5oa.mongodb.net/pastebindb";
+// // process.env.MONGODB_URI;
+
+// if (!MONGODB_URI) {
+//   throw new Error("MONGODB_URI not defined");
+// }
+
+// let cached = global.mongoose;
+
+// if (!cached) {
+//   cached = global.mongoose = { conn: null, promise: null };
+// }
+
+// export async function connectDB() {
+//   if (cached.conn) return cached.conn;
+
+//   if (!cached.promise) {
+//     cached.promise = mongoose.connect(MONGODB_URI, {
+//       bufferCommands: false,
+//     });
+//   }
+
+//   cached.conn = await cached.promise;
+//   return cached.conn;
+// }
 import mongoose from "mongoose";
 
-const MONGODB_URI = "mongodb+srv://subhangipriya681:Password@cluster0.qe5oa.mongodb.net/pastebindb"
-// process.env.MONGODB_URI;
+const MONGODB_URI = "mongodb+srv://subhangipriya681:Password@cluster0.qe5oa.mongodb.net/pastebindb";
 
 if (!MONGODB_URI) {
   throw new Error("MONGODB_URI not defined");
@@ -17,9 +43,16 @@ export async function connectDB() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      bufferCommands: false,
-    });
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        bufferCommands: false,
+        serverSelectionTimeoutMS: 5000,
+      })
+      .then((mongoose) => mongoose)
+      .catch((err) => {
+        cached.promise = null;
+        throw err;
+      });
   }
 
   cached.conn = await cached.promise;
